efe87f21e0fae770f803ef486488e37d1bd3644f
Hristomira Sultova <hsultova@users.noreply.github.com>
2026-05-23 01:56:35 +0300

    Project Setup Add Database & Models CRUD Operations for todos User Interface for todos

---
+++ Agentic-Coding-IDE-Extensions/TodoApp/Controllers/TodosController.cs
@@ -0,0 +1,118 @@
+using Microsoft.AspNetCore.Mvc;
+using TodoApp.Data;
+using TodoApp.Models;
+
+namespace TodoApp.Controllers
+{
+    public class TodosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public TodosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Todos/Index
+        public async Task<IActionResult> Index()
+        {
+            var todos = _context.Todos.OrderBy(t => t.CreatedAt).ToList();
+            return View(todos);
+        }
+
+        // GET: Todos/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Todos/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Title,Description,DueDate")] Todo todo)
+        {
+            if (ModelState.IsValid)
+            {
+                todo.CreatedAt = DateTime.UtcNow;
+                todo.IsDone = false;
+                _context.Add(todo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(todo);
+        }
+
+        // GET: Todos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var todo = await _context.Todos.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            return View(todo);
+        }
+
+        // POST: Todos/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,IsDone,CreatedAt,DueDate")] Todo todo)
+        {
+            if (id != todo.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(todo);
+                    await _context.SaveChangesAsync();
+                }
+                catch
+                {
+                    ModelState.AddModelError("", "An error occurred while updating the todo.");
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(todo);
+        }
+
+        // GET: Todos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var todo = await _context.Todos.FindAsync(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return View(todo);
+        }
+
+        // POST: Todos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var todo = await _context.Todos.FindAsync(id);
+            if (todo != null)
+            {
+                _context.Todos.Remove(todo);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ Agentic-Coding-IDE-Extensions/TodoApp/Models/Todo.cs
@@ -0,0 +1,12 @@
+namespace TodoApp.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool IsDone { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DueDate { get; set; }
+    }
+}
